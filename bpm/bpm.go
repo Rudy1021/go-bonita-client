4c1d46f63f43cd328a498776fedb95594ddc51a7
RudyWang <a86742902@gmail.com>
2022-01-25 03:45:58 +0800

    完成bonitaAPI雛形

--- bpm/bpm.go
+++ bpm/bpm.go
@@ -6,7 +6,7 @@ import (
 	"github.com/go-resty/resty/v2"
 )
 
-var bc *BPMClient
+var Bc *BPMClient
 
 type BPMClient struct {
 	server   string
@@ -22,7 +22,7 @@ func init() {
 	// 	// os.Getenv("BPM_SERVER_ADDR"),
 	// 	os.Getenv("b.server"),
 	// )
-	bc = &BPMClient{
+	Bc = &BPMClient{
 		server:   server_addr,
 		token:    "",
 		username: "",
@@ -33,10 +33,10 @@ func init() {
 
 // Login
 // /bonita/loginservice
-func Login(username string) {
+func (b *BPMClient) Login(username string) {
 
-	url := bc.server + "loginservice"
-	resp, err := bc.client.R().
+	url := b.server + "loginservice"
+	resp, err := b.client.R().
 		SetFormData(map[string]string{
 			"username": username,
 			"password": "12345",
@@ -47,7 +47,7 @@ func Login(username string) {
 	}
 	for _, cookie := range resp.Cookies() {
 		if cookie.Name == "X-Bonita-API-Token" {
-			bc.token = cookie.Value
+			b.token = cookie.Value
 		}
 	}
 }
@@ -56,14 +56,14 @@ func Login(username string) {
 // /bonita/API/bpm/process/[ProcessId]/instantiation
 // [ProcessId] == 表單編號
 // return caseId
-func StartForm(processID string, body string) string {
+func (b *BPMClient) StartForm(processID string, body string) string {
 
-	url := bc.server + "API/bpm/process/" + processID + "/instantiation"
+	url := b.server + "API/bpm/process/" + processID + "/instantiation"
 
-	resp, err := bc.client.R().
+	resp, err := b.client.R().
 		SetHeaders(map[string]string{
 			"Content-Type":       "application/json",
-			"X-Bonita-API-Token": bc.token,
+			"X-Bonita-API-Token": b.token,
 		}).
 		SetBody(body).
 		Post(url)
@@ -73,3 +73,82 @@ func StartForm(processID string, body string) string {
 
 	return string(resp.Body())
 }
+
+func (b *BPMClient) GetReadyCase(c string, state string, user_id string) string {
+
+	url := b.server + "API/bpm/humanTask?c=" + c + "&f=state=" + state + "&f=user_id=" + user_id
+	resp, err := b.client.R().
+		Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(resp.Body())
+}
+
+func (b *BPMClient) GetDetailCase(case_id string) string {
+
+	url := b.server + "API/bpm/humanTask?f=caseId=" + case_id
+	resp, err := b.client.R().
+		Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(resp.Body())
+}
+
+func (b *BPMClient) GetFinishCase(case_id string) string {
+	//
+	url := b.server + "API/bpm/archivedTask?f=caseId=" + case_id
+	resp, err := b.client.R().
+		Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(resp.Body())
+}
+
+func (b *BPMClient) GetFinishCaseState(sourceObjectId string) string {
+	//
+	url := b.server + "API/bpm/archivedHumanTask?f=sourceObjectId=" + sourceObjectId
+	resp, err := b.client.R().
+		Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(resp.Body())
+}
+
+func (b *BPMClient) GetAllProcessCase(c string, processId string) string {
+	//
+	url := b.server + "API/bpm/case?c=" + c + "&f=processDefinitionId=" + processId
+	resp, err := b.client.R().
+		Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(resp.Body())
+}
+
+func (b *BPMClient) ReviewCase(task_id string, body string) string {
+	//
+	url := b.server + "API/bpm/userTask/" + task_id + "/execution?assign=true"
+	resp, err := b.client.R().
+		SetHeaders(map[string]string{
+			"Content-Type":       "application/json",
+			"X-Bonita-API-Token": b.token,
+		}).
+		SetBody(body).
+		Post(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(resp.Body())
+}
+
+//userTask/(TaskId)/execution?assign=true
--- main.go
+++ main.go
@@ -7,8 +7,9 @@ import (
 )
 
 func main() {
-	bpm.Login("isabelle_wu")
-	body := bpm.StartForm("8759976868088592450", `{
+	client := bpm.Bc
+	client.Login("isabelle_wu")
+	body := client.StartForm("8759976868088592450", `{
 		"modelInput":
 		{
 				"assistant":"choc",
@@ -16,4 +17,33 @@ func main() {
 		}
 }`)
 	fmt.Println(body)
+
+	res := client.GetReadyCase("50", "ready", "38")
+	fmt.Println(res)
+
+	res = client.GetDetailCase("3075")
+	fmt.Println(res)
+
+	//res = client.GetFinishCase("3075")
+	//fmt.Println(res)
+
+	//res = client.GetFinishCaseState("3075")
+	//fmt.Println(res)
+
+	/*res = client.ReviewCase("", `{
+		"modelInput":{
+			"gmApprovalStatus":最高主管審核狀態 : boolean
+		}
+	}`)
+	fmt.Println(res)
+
+	res = client.ReviewCase("", `{
+	  "modelInput":{
+	  	"dmApprovalStatus":部門主管審核狀態 : boolean
+	  }
+	}`)
+	fmt.Println(res)
+	*/
+	res = client.GetAllProcessCase("20", "8759976868088592450")
+	fmt.Println(res)
 }
